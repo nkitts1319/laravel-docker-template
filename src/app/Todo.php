c722fa57fa313e1bac5c70b9c628748aea5f9e9a
nkitts1319 <nkitts1319@gmail.com>
2025-01-28 05:31:33 +0900

    S14 データの登録処理の保守性を高める方法

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -8,6 +8,10 @@ class Todo extends Model
 {
     // Modelを介することでSQL文を組み立てることなくtodosテーブルを操作することができるようになる。
     protected $table = 'todos';
+
+    protected $fillable = [
+        'content',
+    ];
 }
 
 /* php artisan make:model Todo コマンドを実行するとファイルの中に下記のコードが入っている。
