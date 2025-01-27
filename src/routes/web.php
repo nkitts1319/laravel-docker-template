7269e5b4d1d6d9d2ce833a4782ffe34f03d676c1
nkitts1319 <nkitts1319@gmail.com>
2025-01-27 21:01:38 +0900

    S12 Route,Viewのリファクタリング

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
 
 // 編集
 Route::get('/todo', 'TodoController@index');
-Route::get('/todo/create', 'TodoController@create'); // S11 追記
+Route::get('/todo/create', 'TodoController@create')->name('todo.create'); // 追記; // S11 追記
